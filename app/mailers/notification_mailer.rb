6888841365f1644a992950fd61ff3f5b50d241f4
Victoria Brouard <victoria_brouard@hotmail.co.uk>
2015-12-30 08:55:02 +0000

    Email include place info and link

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,12 @@
 class NotificationMailer < ActionMailer::Base
   default from: "from@example.com"
 
-  def comment_added
-  	mail(to:"victoria_brouard@hotmail.co.uk",
-  		subject: "A comment has been added to your place")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to #{@place.name}")
   end
 
 end
