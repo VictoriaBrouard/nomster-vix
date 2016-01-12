08a740998898f6b8c326304a64d9334300eed19e
Victoria Brouard <victoria_brouard@hotmail.co.uk>
2016-01-12 08:16:13 +0000

    User Dashboard Test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	include Devise::TestHelpers
+
+	test "user_dashboard"
+	@request.env["devise.mapping"] = Devise.mappings[:user]
+		user = sign _in FactoryGirl.create(:user)
+		get :show, :id => user.id
+		assert_response :success
+  end
 end
